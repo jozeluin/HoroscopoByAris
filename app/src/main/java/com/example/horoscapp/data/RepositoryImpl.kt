26eb1f5b59edffab852f8efd2064e99b93ee5573
jozel <jozeluin@gmail.com>
2023-11-09 12:39:11 +0100

    Pc.Trabajar con estados, consumo apis. Dagger. Creacion de modulos para librerias y interfaces en dagger. 4:18

--- app/src/main/java/com/example/horoscapp/data/RepositoryImpl.kt
+++ app/src/main/java/com/example/horoscapp/data/RepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.example.horoscapp.data
 
+import com.example.horoscapp.data.network.HoroscopeApiService
 import com.example.horoscapp.domain.Repository
+import javax.inject.Inject
 
-class RepositoryImpl:Repository {
+class RepositoryImpl @Inject constructor(private val apiService:HoroscopeApiService):Repository {
+
     override suspend fun getPrediction(sign: String) {
-
+        //PeticionRetrofit
     }
 }
---
+++ app/src/main/java/com/example/horoscapp/data/network/NetworkModule.kt
@@ -0,0 +1,35 @@
+package com.example.horoscapp.data.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+
+       return Retrofit
+            .Builder()
+            .baseUrl("https://newastro.vercel.app/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+    }
+
+    @Provides
+    fun provideHoroscopeApiServices(retrofit: Retrofit):HoroscopeApiService{
+        return retrofit.create(HoroscopeApiService::class.java)
+
+    }
+
+}
