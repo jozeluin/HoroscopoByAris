162f76b0d65668d80baf09a80bc9e1da6ab7d5f9
jozel <jozeluin@gmail.com>
2023-11-22 00:24:21 +0100

    Pc.6:56 Implementaciones necesarias + Permisos camara en PalmistryFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ android {
 dependencies {
 
     val navVersion= "2.7.1"
+    val cameraVersion = "1.2.3"
 
     //NavComponent
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
@@ -97,4 +98,28 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.3.1")
+
+    //Camera X
+    implementation ("androidx.camera:camera-core:${cameraVersion}")
+    implementation ("androidx.camera:camera-camera2:${cameraVersion}")
+    implementation ("androidx.camera:camera-lifecycle:${cameraVersion}")
+    implementation ("androidx.camera:camera-view:${cameraVersion}")
+    implementation ("androidx.camera:camera-extensions:${cameraVersion}")
+
+    //UnitTesting
+    testImplementation("junit:junit:4.13.2")
+    testImplementation ("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+    testImplementation ("io.mockk:mockk:1.12.3")
+
+    //UITesting
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-intents:3.4.0")
+    androidTestImplementation ("com.google.dagger:hilt-android-testing:2.48")
+    androidTestImplementation ("androidx.fragment:fragment-testing:1.6.1")
+    kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.48")
+
+
+
 }
--- app/src/main/java/com/example/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.example.horoscapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,9 +14,45 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
+    companion object{
+        private const val CAMERA_PERMISION = android.Manifest.permission.CAMERA
+    }
 
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher=registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted->
+        if(isGranted){
+            //startCamera
+        }else{
+            Toast.makeText(requireContext(),"Acepta los permisos para poder disfrutar de " +
+                    "una experiencia magica",
+                Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(checkCameraPermission()){
+            //tiene permisos aceptados
+
+        }else{
+            requestPermissionLauncher.launch(CAMERA_PERMISION)
+
+        }
+
+    }
+
+    fun checkCameraPermission():Boolean{
+        return PermissionChecker.checkSelfPermission(
+            requireContext(),
+            CAMERA_PERMISION
+        )== PermissionChecker.PERMISSION_GRANTED
+    }
 
 
     override fun onCreateView(
