03250d3c9ad47f2c2921cde3217a18dcb02e0da0
jozel <jozeluin@gmail.com>
2023-11-23 07:41:53 +0100

    Pc. Hemos acabado de hacer los test del Ui. Hemos desactivado en la ultima prueba las animaciones del movil, hemos habilitado las opciones de desarrollador del emulador, por que si no, no le daba tiempo al test.

--- app/src/test/java/com/example/horoscapp/ui/horoscope/HorosopeViewModelTest.kt
+++ app/src/test/java/com/example/horoscapp/ui/horoscope/HorosopeViewModelTest.kt
@@ -1,21 +1,37 @@
 package com.example.horoscapp.ui.horoscope
 
-import org.junit.Assert.*
+import com.example.horoscapp.data.providers.HoroscopeProvider
+import com.example.horoscapp.motherObject.HoroscopeMotherObject.horoscopeInfoList
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+
 import org.junit.Before
 import org.junit.Test
+import org.junit.Assert.*
 
 class HorosopeViewModelTest{
 
+    @MockK
+    lateinit var horoscopeProvider: HoroscopeProvider
+
     private lateinit var viewModel: HorosopeViewModel
+
     @Before
     fun setup(){
+        MockKAnnotations.init(this, relaxUnitFun = true)
 
 
     }
 
     @Test
     fun `when viewModel is created then horoscopes are loaded`(){
-       // viewModel=HorosopeViewModel()
+        every{horoscopeProvider.getHoroscope()} returns horoscopeInfoList
+       viewModel=HorosopeViewModel(horoscopeProvider)
+
+        val horoscopes=viewModel.horoscope.value
+
+        assertTrue(horoscopes.isNotEmpty())
     }
 
 }
